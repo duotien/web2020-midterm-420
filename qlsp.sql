2f8fbde661a869c38f189bd21ca20c31e8967b4f
Tien Duong <tienmum@gmail.com>
2020-07-04 09:18:13 +0700

    Thêm tất cả các bảng

--- qlsp.sql
+++ qlsp.sql
@@ -23,6 +23,36 @@ CREATE TABLE IF NOT EXISTS HANGHOA (
     MaHH VARCHAR(10) COLLATE utf8_unicode_ci NOT NULL,
     TenHH VARCHAR(50) COLLATE utf8_unicode_ci NOT NULL,
     DonGia INT(11) NOT NULL,
-    Hinh varchar(50) COLLATE utf8_unicode_ci
+    Hinh varchar(50) COLLATE utf8_unicode_ci,
+    PRIMARY KEY (MaHH)
 );
 
+#KhachHang(#MaKH, HoTen, DienThoai, Email)
+CREATE TABLE IF NOT EXISTS KHACHHANG (
+    MaKH VARCHAR(10) COLLATE utf8_unicode_ci NOT NULL,
+    HoTen VARCHAR(50) COLLATE utf8_unicode_ci NOT NULL,
+    DienThoai VARCHAR(15) COLLATE utf8_unicode_ci NULL,
+    Email VARCHAR(50) COLLATE utf8_unicode_ci NULL,
+    PRIMARY KEY (MaKH)
+);
+
+#HoaDon(#MaHD, NgayLapHD, MaKH, TongTien)
+CREATE TABLE IF NOT EXISTS HOADON (
+    MaHD VARCHAR(10) COLLATE utf8_unicode_ci NOT NULL,
+    NgayLapHD Datetime NOT NULL,
+    MaKH VARCHAR(10) COLLATE utf8_unicode_ci NULL,
+    TongTien INT(11),
+    PRIMARY KEY (MaHD),
+    CONSTRAINT FOREIGN KEY (MaKH) REFERENCES KHACHHANG(MaKH)
+);
+
+#ChiTietHoaDon(#MaCTHD, MaHD, MaHH, SoLuong)
+CREATE TABLE IF NOT EXISTS CHITIETHOADON (
+    MaCTHD VARCHAR(10) COLLATE utf8_unicode_ci NOT NULL,
+    MaHD VARCHAR(10) COLLATE utf8_unicode_ci NOT NULL,
+    MaHH VARCHAR(10) COLLATE utf8_unicode_ci NULL,
+    SoLuong INT(11),
+    PRIMARY KEY (MaCTHD),
+    CONSTRAINT FOREIGN KEY (MaHD) REFERENCES HOADON(MaHD),
+    CONSTRAINT FOREIGN KEY (MaHH) REFERENCES HANGHOA(MaHH)
+);
